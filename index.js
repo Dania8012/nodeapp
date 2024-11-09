701f892acb9bb961992e04a70025a3fea13162e0
Dania Alrefai <daniaalrefai@BGIT00058.local>
2024-11-09 13:02:08 +0400

    Random commit

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   console.log("Sending new logs");
-  res.status(200).json({res: "Test response - new version"});
+  res.status(200).json({res: "Test response - new version2"});
 });
 
 app.get("/test", (req, res) => {
   console.log("Sending new logs");
-  res.status(200).json({res: "Test respons - new version"});
+  res.status(200).json({res: "Test respons - new version2"});
 });
 
 app.listen(port, () => {
   console.log(`App is listening at http://localhost:${port}`);
-});
+}); 
